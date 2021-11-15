95a1ff51c17f5d3493124ff18e129807ce8c5c21
zugzw4ng <bongcloud2001@gmail.com>
2021-11-15 17:58:59 -0500

    added save button functionality setting with each hour block field's ID

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,26 +12,31 @@ function getSavedInput() {
 // Change hour block colors based on current time (using Moment)
 function colorCodeBlocks() {
     $(".inputField").each(function () {
-    const currentHour = moment().format("H")
-    const blockHour = parseInt($(this).attr("id"))
+        const currentHour = moment().format("H")
+        const blockHour = parseInt($(this).attr("id"))
 
-    if (blockHour < currentHour) {
-        $(this).addClass("past");
-        $(this).removeClass("present");
-        $(this).removeClass("future");
-    }
-    else if (blockHour == currentHour) {
-        $(this).addClass("present");
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-    }
-    else {
-        $(this).addClass("future");
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-    }
-})
-}
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+        else if (blockHour == currentHour) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+    })
+};
 // save button functionality and localstorage save
+$(".saveButton").on("click", function () {
+    const userInput = $(this).siblings(".inputField").val();
+    const timeID = $(this).siblings(".inputField").attr("id");
 
+    localStorage.setItem(timeID, userInput);
+})
 // function calls
