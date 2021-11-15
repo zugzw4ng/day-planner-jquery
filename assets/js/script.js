ae53ed55c1353b36138882e9512e6fcf3e3dcbb1
zugzw4ng <bongcloud2001@gmail.com>
2021-11-15 17:53:33 -0500

    added variables for comparing hour blocks to current time in hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,17 @@ $("#currentDay").text(moment().format("dddd, MMM Do, YYYY"));
 // Initialize data from localstorage
 function getSavedInput() {
     $(".inputField").each(function () {
-        var inputValue = $(this).attr("id");
+        const inputValue = $(this).attr("id");
         $(this).val(localStorage.getItem(inputValue));
     }
     )
-}
+};
 // Change hour block colors based on current time (using Moment)
-
+function colorCodeBlocks() {
+    $(".inputField").each(function () {
+    const currentHour = moment().format("H")
+    const blockHour = parseInt($(this).attr("id"))
+}
 // save button functionality and localstorage save
 
 // function calls
