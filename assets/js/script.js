423e7de4ca74bd87ffa21d02a1c43f3a31a3a82a
zugzw4ng <bongcloud2001@gmail.com>
2021-11-15 17:47:24 -0500

    added current header date with moment, getItem method for saved localstorage data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 // Display Todays Date in the Header
+$("#currentDay").text(moment().format("dddd, MMM Do, YYYY"));
 
 // Initialize data from localstorage
-
+function getSavedInput() {
+    $(".inputField").each(function () {
+        var inputValue = $(this).attr("id");
+        $(this).val(localStorage.getItem(inputValue));
+    }
+    )
+}
 // Change hour block colors based on current time (using Moment)
 
 // save button functionality and localstorage save
