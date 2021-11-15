c6c9562b84043c937b07940ed30fc156959be88d
zugzw4ng <bongcloud2001@gmail.com>
2021-11-15 17:46:03 -0500

    added pseudo code for script file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Display Todays Date in the Header
+
+// Initialize data from localstorage
+
+// Change hour block colors based on current time (using Moment)
+
+// save button functionality and localstorage save
+
+// function calls
